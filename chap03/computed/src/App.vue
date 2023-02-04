45a8e81f5056ebd9bc76c69f96adc18e7b756e7c
seiyaKM <seiya.kumada.0107@gmail.com>
2023-02-04 21:52:10 +0900

    ~ p.42

---
+++ chap03/computed/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed, ref, reactive } from 'vue';
+
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+})
+
+// const radiusInit = Math.round(Math.random() * 10);
+// const PI = ref(3.14);
+
+// const radius = ref(radiusInit);
+// const area = computed(
+//   (): number => {
+//     return radius.value * radius.value * PI.value;
+//   }
+// );
+const area = computed(
+  (): number => {
+    return data.radius * data.radius * data.PI;
+  }
+);
+
+// setInterval(
+//   ():void => {
+//     radius.value = Math.round(Math.random() * 10);
+//   },3000
+// )
+setInterval(
+  ():void => {
+    data.radius = Math.round(Math.random() * 10);
+  },3000
+)
+
+</script>
+
+<template>
+  <p>半径{{data.radius}}の円の面積を円周率{{data.PI}}で計算すると、{{area}}</p>
+</template>
---
+++ chap03/reactive-system/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+import { ref } from "vue";
+
+const now = new Date();
+const nowStr = now.toLocaleTimeString();
+
+let timeStr = nowStr;
+
+const timeStrRef = ref(nowStr);
+
+function changeTim(): void {
+  const newTime = new Date();
+  const newTimeStr = newTime.toLocaleTimeString();
+
+  timeStr = newTimeStr;
+
+  timeStrRef.value = newTimeStr;
+}
+
+setInterval(changeTim, 1000);
+</script>
+
+<template>
+  <p>time string : {{ timeStr }}</p>
+  <p>time string ref : {{ timeStrRef }}</p>
+</template>
