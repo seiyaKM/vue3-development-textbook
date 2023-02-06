4e23d930c630b203bab9d8107646a999fa950412
seiyaKM <seiya.kumada.0107@gmail.com>
2023-02-07 06:42:05 +0900

    ~ p.96

---
+++ chap04/directive-on-param-event/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const mousePointX = ref(0);
+const mousePointY = ref(0);
+
+const onImgMousemove = (event: MouseEvent): void => {
+  mousePointX.value = event.offsetX;
+  mousePointY.value = event.offsetY;
+};
+</script>
+
+<template>
+  <img src="./assets/logo.svg" alt="Vue Logo" width="200" @mousemove="onImgMousemove">
+  <p>pointer point : x = {{ mousePointY }}; y = {{ mousePointY }}</p>
+</template>
---
+++ chap05/directive-model/src/App.vue
@@ -0,0 +1,35 @@
+<!-- <script setup lang="ts">
+import { ref } from 'vue';
+
+const inputNameModel = ref("双方入力");
+</script>
+
+<template>
+  <section>
+    <input type="text" v-model="inputNameModel">
+    <p>{{ inputNameModel }}</p>
+  </section>
+</template> -->
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const inputNameBind = ref('shin');
+const inputNameOn = ref('no name');
+
+const onInputName = (event: Event): void => {
+  const element = event.target as HTMLInputElement;
+  inputNameOn.value = element.value
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputNameBind">
+  </section>
+  <br>
+  <section>
+    <input type="text" v-on:input="onInputName">
+    <p>{{ inputNameOn }}</p>
+  </section>
+</template>
