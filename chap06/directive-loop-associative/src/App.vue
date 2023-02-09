2b7e1e0f2f4350b835be2e5922037b29a962ad77
seiyaKM <seiya.kumada.0107@gmail.com>
2023-02-09 22:54:51 +0900

    ~ p.136

---
+++ chap06/directive-loop-associative/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const artistsInit: { [key: number]: string } = {
+  100: "ELLEGARDEN",
+  200: "HIATUS",
+  300: "MONOEYES",
+};
+const artists = ref(artistsInit);
+</script>
+
+<template>
+  <ul>
+    <li v-for="(artist, id) in artists" :key="'artists' + id">
+      ID is {{ id }}, Artist is {{ artist }}
+    </li>
+  </ul>
+  <ul>
+    <li v-for="(artist, id, index) in artists" :key="'artists_with_index' + id">
+      {{ index }}: ID is {{ id }}, Artist is {{ artist }}
+    </li>
+  </ul>
+</template>
