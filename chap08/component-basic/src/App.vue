034e67e4ed366238fb3bef13bd50eba08d50bf86
seiyaKM <seiya.kumada.0107@gmail.com>
2023-02-12 06:25:24 +0900

    ~p.170

---
+++ chap08/component-basic/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import OneSection from "@/components/OneSection.vue";
+</script>
+
+<template>
+  <h1>Component Basic</h1>
+  <section>
+    <h2>one component</h2>
+    <OneSection />
+  </section>
+  <section>
+    <h2>multi component</h2>
+    <OneSection />
+    <OneSection />
+    <OneSection />
+  </section>
+</template>
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
---
+++ chap08/component-basic/src/components/OneSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="box">
+    <h4>one component</h4>
+    <p>component is ...</p>
+  </section>
+</template>
+
+<style>
+.box {
+  border: green 1px dashed;
+  margin: 10px;
+}
+</style>
