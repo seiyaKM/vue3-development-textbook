e827a2fd4e8de173bf2e8869133f368280d72e10
seiyaKM <seiya.kumada.0107@gmail.com>
2023-02-22 06:54:14 +0900

    ~ p.197

---
+++ chap08/components-emit-basic/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const rand = ref(Math.round(Math.random() * 10));
+const onCreateNewRand = (): void => {
+  rand.value = Math.round(Math.random() * 10);
+};
+</script>
+
+<template>
+  <section>
+    <p>state rand value : {{ rand }}</p>
+    <OneSection :rand="rand" @create-new-rand="onCreateNewRand" />
+  </section>
+</template>
---
+++ chap08/components-emit-basic/src/components/OneSection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+type Props = {
+  rand: number;
+};
+
+type Emits = {
+  (event: "createNewRand"): void;
+};
+
+defineProps<Props>();
+
+const emit = defineEmits<Emits>();
+
+const onNewRandButtonClick = (): void => {
+  emit("createNewRand");
+};
+</script>
+
+<template>
+  <section class="box">
+    <p>props rand value : {{ rand }}</p>
+    <button @click="onNewRandButtonClick">create rand</button>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
