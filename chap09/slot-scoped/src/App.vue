0de0f288b6335966eaf840371f486cc43a6d3e14
seiyaKM <seiya.kumada.0107@gmail.com>
2023-02-24 07:07:09 +0900

    ~ p.246

---
+++ chap09/slot-scoped/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue'
+</script>
+
+<template>
+  <section>
+    <OneSection>
+      <template #default="slotProps">
+        <dl>
+          <dt>Name</dt>
+          <dd>{{ slotProps.memberInfo.name }}</dd>
+          <dt>Status</dt>
+          <dd>{{ slotProps.memberInfo.state }}</dd>
+        </dl>
+      </template>
+    </OneSection>
+    <OneSection />
+  </section>
+</template>
---
+++ chap09/slot-scoped/src/components/OneSection.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const memberInfo = reactive({ name: 'steve', state: 'no problem' })
+</script>
+
+<template>
+  <section>
+    <slot :memberInfo="memberInfo">
+      <h1>{{ memberInfo.name }} state</h1>
+      <p>{{ memberInfo.state }}</p>
+    </slot>
+  </section>
+</template>
